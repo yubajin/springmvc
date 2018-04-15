dfc3a8073aa2cc32f7bf5018d4f53810660b8c22
yubajin <1803279085@qq.com>
2018-04-15 23:31:07 +0800

    add pojo对象绑定

---
+++ src/com/yubajin/springmvc/entity/User.java
@@ -0,0 +1,50 @@
+package com.yubajin.springmvc.entity;
+
+public class User {
+	private String username;
+	private String password;
+	
+	private String email;
+	
+	private Address address;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", email=" + email + ", address=" + address
+				+ "]";
+	}
+	
+	
+}
